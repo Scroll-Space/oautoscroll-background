51b300b076f04ca9d777ce49e4c398b969e1c31e
moprules <rav-navini-gego-cutropal@yandex.ru>
2026-08-21 23:19:25 +0300

    working autoscron on open pdf

--- scripts/plugin.js
+++ scripts/plugin.js
@@ -4,45 +4,39 @@ import { EditorController } from './scroll-core/m_editor.js';
 const plugStore = new PluginStorage(window);
 const editorCtrl = new EditorController(window);
 
-function tryRestoreScroll() {
-    // if (isRestored) return;
+window.Asc.plugin.init = function () {
+    tryRestoreScroll();
+    startTempScrollSaver();
+};
 
-    if (plugStore.getMoveByOpenFlag() && !plugStore.isFirstOpen()) {
-        const savedY = plugStore.getScroll();
-        if (savedY !== null) {
-            console.log("📍 [Backend] Восстанавливаем позицию Y:", savedY);
-            editorCtrl.moveScroll(savedY);
-            // isRestored = true;
+// Минималистичный таймер, который почти не тратит CPU
+function startTempScrollSaver() {
+    setInterval(() => {
+        if (!plugStore.getSaveByCloseFlag()) return;
+
+        const currentY = editorCtrl.getScrollY();
+
+        if (currentY === null) return;
+
+        let tempSavedY = plugStore.getTempScroll();
+
+        if (tempSavedY === null || tempSavedY !== currentY) {
+            plugStore.saveTempScroll(currentY);
         }
-    }
+    }, 1000);
 }
 
-window.Asc.plugin.init = function () {
-    console.log("🔄 [Backend] Init вызвался");
-    console.log("🔄 [Backend] plugStore.getMoveByOpenFlag():", plugStore.getMoveByOpenFlag());
-    console.log("🔄 [Backend] plugStore.getScroll():", plugStore.getScroll());
+function tryRestoreScroll() {
+    if (plugStore.getMoveByOpenFlag() && !plugStore.isFirstOpen()) {
+        let savedY = plugStore.getScroll();
+        let tempSavedY = plugStore.getTempScroll();
 
-    // Пытаемся запросить параметры файла. 
-    // В коллбэке мы Твёpдо знаем, что документ загружен и готов!
-    this.executeMethod("GetFileParams", [], function (params) {
-        console.log("✅ [Backend] Документ точно готов!", params);
-        // tryRestoreScroll();
-    });
-};
-
-// 2. Прямой перехват события SDK ONLYOFFICE (без attachEditorEvent)
-window.Asc.plugin.event_onDocumentContentReady = function () {
-    console.log("📄 [Backend] Event onDocumentContentReady сработал");
-    tryRestoreScroll();
-};
-
-// 3. Сохранение при закрытии редактора/вкладки
-window.addEventListener("beforeunload", () => {
-    if (plugStore.getSaveByCloseFlag()) {
-        const curScrollY = editorCtrl.getScrollY();
-        if (curScrollY !== null) {
-            console.log("🔒 [Backend] Сохраняем позицию Y:", curScrollY);
-            plugStore.saveScroll(curScrollY);
+        // При загрузке плагина, если есть временный скролл, то он становится основным
+        if (tempSavedY !== null && tempSavedY !== savedY && plugStore.getSaveByCloseFlag()) {            
+            savedY = tempSavedY;
+            plugStore.saveScroll(savedY);
         }
+        editorCtrl.moveScroll(savedY);
+
     }
-});
+}
